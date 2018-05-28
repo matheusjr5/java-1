d6518e0c16f843c7a5b11c05c5e1ee52e052f5eb
kevin.scosenza <kevin.scosenza@TAT0541230W10-1.senacsp.edu.br>
2018-05-28 16:24:03 -0300

    Adicionei a classe CalculoImc

---
+++ Fundamentos do Java/src/br/com/java/CalculoImc.java
@@ -0,0 +1,27 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+public class CalculoImc {
+
+	public static void main(String[] args) {
+		// a linha abaixo cria variáveis numéricas de precisão
+		// float e double são tipos primitivos numéricos
+		double peso,altura,imc;
+		// entrada
+		System.out.print("Digite seu peso em kg: ");
+		// a linha abaixo cria um objeto de nome teclado usando o Scanner
+		Scanner teclado = new Scanner(System.in);
+		// a linha abaixo usa o objeto teclado para capturar o que foi digitado
+		// digitando no console e armazenar no variável peso
+		peso = teclado.nextDouble();
+		System.out.print("Digite a sua altura em metros: ");
+		altura = teclado.nextDouble();
+		// processamento 
+		imc = peso/(altura * altura);
+		//saida
+		// o / divide o peso pela altura
+		// o * multiplica a alutra pela altura
+		System.out.println("resultado de imc : " + imc);	
+	}
+}
