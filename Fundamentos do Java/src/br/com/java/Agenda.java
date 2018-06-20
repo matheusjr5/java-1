868aeea253b4910d356717b6f7b72b923c7e45d0
kevin.scosenza <kevin.scosenza@TAT0541230W10-1.senacsp.edu.br>
2018-06-20 14:00:07 -0300

    att

--- Fundamentos do Java/src/br/com/java/Agenda.java
+++ Fundamentos do Java/src/br/com/java/Agenda.java
@@ -4,7 +4,7 @@ public class Agenda {
 
 	public static void main(String[] args) {
 		String [][] agenda = {{"bill gates","1111-1111", "bill@outlok.com"},{"linus torvalds", "2222-2222", "linus@gmail.com"}, {"steve jobs", "3333-3333", "steve@icloud.com"}};
-		//recuperando o email do linus torvals
+		//recuperando o email do linus torvalds
 		//System.out.println("email encontrado + agenda [1][2]);
 		//alterando o telefone do bill gates
 		agenda [0][1] = "9999-9999";
